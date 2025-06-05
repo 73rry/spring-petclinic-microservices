60c943cd6c1d0b18434c324623776efd632fe654
Nguyen Thanh Tuan <tuannt.ti.1922@gmail.com>
2025-06-05 15:04:40 +0700

    Add actuator to admin / config / discovery server

--- build-k8s-image.sh
+++ build-k8s-image.sh
@@ -2,7 +2,7 @@
 
 export REPOSITORY_PREFIX="terrytantan"
 export SPRING_PROFILES_ACTIVE="k8s"
-export VERSION=1278912
+export VERSION=latest
 mvn clean install -Dmaven.test.skip -P buildDocker -Ddocker.image.prefix=${REPOSITORY_PREFIX} 
 ./scripts/tagImages.sh
 ./scripts/pushImages.sh
