ffae11bb2735c4b8ed4e8117b4b6db44f2487b9b
Nguyen Thanh Tuan <tuannt.ti.1922@gmail.com>
2025-06-05 18:31:00 +0700

    Add genai service to push script

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -5,4 +5,5 @@ docker push ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway:${VERSION}
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-visits-service:${VERSION}
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-vets-service:${VERSION}
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-customers-service:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-genai-service:${VERSION}
 docker push ${REPOSITORY_PREFIX}/spring-petclinic-admin-server:${VERSION}
