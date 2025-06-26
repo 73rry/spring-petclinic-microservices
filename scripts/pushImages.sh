e8203fba7e07de79de52f812081254d01206b501
Nguyen Thanh Tuan <22120405@student.hcmus.edu.vn>
2025-06-27 02:11:26 +0700

    fix wrong format

--- scripts/pushImages.sh
+++ scripts/pushImages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-config-server-{$ENV}:${VERSION}
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-discovery-server-{$ENV}:${VERSION}
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway-{$ENV}:${VERSION}
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-visits-service-{$ENV}:${VERSION}
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-vets-service-{$ENV}:${VERSION}
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-customers-service-{$ENV}:${VERSION}
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-genai-service-{$ENV}:${VERSION}
-docker push ${REPOSITORY_PREFIX}/spring-petclinic-admin-server-{$ENV}:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-config-server-$ENV:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-discovery-server-$ENV:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway-$ENV:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-visits-service-$ENV:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-vets-service-$ENV:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-customers-service-$ENV:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-genai-service-$ENV:${VERSION}
+docker push ${REPOSITORY_PREFIX}/spring-petclinic-admin-server-$ENV:${VERSION}
--- scripts/tagImages.sh
+++ scripts/tagImages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker tag ${REPOSITORY_PREFIX}/spring-petclinic-config-server ${REPOSITORY_PREFIX}/spring-petclinic-config-server-{$ENV}:${VERSION}
-docker tag ${REPOSITORY_PREFIX}/spring-petclinic-discovery-server ${REPOSITORY_PREFIX}/spring-petclinic-discovery-server-{$ENV}:${VERSION}
-docker tag ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway-{$ENV}:${VERSION}
-docker tag ${REPOSITORY_PREFIX}/spring-petclinic-visits-service ${REPOSITORY_PREFIX}/spring-petclinic-visits-service-{$ENV}:${VERSION}
-docker tag ${REPOSITORY_PREFIX}/spring-petclinic-vets-service ${REPOSITORY_PREFIX}/spring-petclinic-vets-service-{$ENV}:${VERSION}
-docker tag ${REPOSITORY_PREFIX}/spring-petclinic-customers-service ${REPOSITORY_PREFIX}/spring-petclinic-customers-service-{$ENV}:${VERSION}
-docker tag ${REPOSITORY_PREFIX}/spring-petclinic-admin-server ${REPOSITORY_PREFIX}/spring-petclinic-admin-server-{$ENV}:${VERSION}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-config-server ${REPOSITORY_PREFIX}/spring-petclinic-config-server-$ENV:${VERSION}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-discovery-server ${REPOSITORY_PREFIX}/spring-petclinic-discovery-server-$ENV:${VERSION}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway ${REPOSITORY_PREFIX}/spring-petclinic-api-gateway-$ENV:${VERSION}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-visits-service ${REPOSITORY_PREFIX}/spring-petclinic-visits-service-$ENV:${VERSION}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-vets-service ${REPOSITORY_PREFIX}/spring-petclinic-vets-service-$ENV:${VERSION}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-customers-service ${REPOSITORY_PREFIX}/spring-petclinic-customers-service-$ENV:${VERSION}
+docker tag ${REPOSITORY_PREFIX}/spring-petclinic-admin-server ${REPOSITORY_PREFIX}/spring-petclinic-admin-server-$ENV:${VERSION}
