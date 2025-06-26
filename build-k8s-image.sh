18b570ea5e31a03a8c6a381b2fee48f5dfdb16ee
Nguyen Thanh Tuan <22120405@student.hcmus.edu.vn>
2025-06-27 01:59:18 +0700

    add env in repo name in docker hub

--- build-k8s-image.sh
+++ build-k8s-image.sh
@@ -3,6 +3,7 @@
 export REPOSITORY_PREFIX="terrytantan"
 export SPRING_PROFILES_ACTIVE="k8s"
 export VERSION=latest
+export ENV=staging
 mvn clean install -Dmaven.test.skip -P buildDocker -Ddocker.image.prefix=${REPOSITORY_PREFIX} 
 ./scripts/tagImages.sh
 ./scripts/pushImages.sh
