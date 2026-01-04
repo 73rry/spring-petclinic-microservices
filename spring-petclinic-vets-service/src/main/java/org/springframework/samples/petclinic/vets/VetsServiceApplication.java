d30488592d4e80f9e436c22389a2a820d953fe86
Nguyen Thanh Tuan <22120405@student.hcmus.edu.vn>
2026-01-04 22:05:28 +0700

    Revert "test: test jenkins ci"
    This reverts commit 4c4400654bf815e0b3700bdf9014dce73cdf16be.

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -21,6 +21,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.samples.petclinic.vets.system.VetsProperties;
 
+
 /**
  * @author Maciej Szarlinski
  */
@@ -31,6 +32,5 @@ public class VetsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VetsServiceApplication.class, args);
-
 	}
 }
